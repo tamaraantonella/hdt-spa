3d2f45e30cb195a336eb0e4fc70df995204baf68
tamaraantonella <tamarafrazzetta@gmail.com>
2022-07-12 21:46:48 -0300

    probando probando

---
+++ js/variables.js
@@ -0,0 +1,8 @@
+const ajies = document.getElementById('aji');
+const exoticos = document.getElementById('exo');
+const huertaComun = document.getElementById('hc');
+const pepinos = document.getElementById('pepinos');
+const tomates = document.getElementById('tom');
+const zapallos = document.getElementById('zap');
+
+const boxes = document.getElementsByClassName('boxes-box')
