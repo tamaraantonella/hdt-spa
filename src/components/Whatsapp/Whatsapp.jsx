3c3578c1b78c7461a6041d76e46c3e12546286ac
tamaraantonella <tamarafrazzetta@gmail.com>
2022-09-05 15:18:23 -0300

    arreglando link

--- src/components/Whatsapp/Whatsapp.jsx
+++ src/components/Whatsapp/Whatsapp.jsx
@@ -6,7 +6,7 @@ import s from   './Whatsapp.module.css'
 export default function Whatsapp() {
   return (
     <div className={s.wp}>
-        <Link to="http://wa.link/0435k6" className={s.link}><BsWhatsapp/></Link>
+        <a href="http://wa.link/0435k6" className={s.link}><BsWhatsapp/></a>
     </div>
   )
 }
